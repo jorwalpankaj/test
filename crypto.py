9db68ffa1319e7549d4649081d3db14052302059
ThinkX Academy <tonyvr53155@gmail.com>
2021-02-01 22:48:48 +0530

    Update crypto.py

--- crypto.py
+++ crypto.py
@@ -8,12 +8,17 @@ key = get_random_bytes(32)
 iv = Random.new().read(AES.block_size)
 plaintext = b'11234567812345678234567812345678'
 
-Encrypt_obj = AES.new(key, AES.MODE_CBC, iv)
+Encrypt_obj = AES.new(key, AES.MODE_CFB, iv)
+Encrypt_obj1 = AES.new(key, AES.MODE_CBC, iv)
 
-cyphertext1 = iv + Encrypt_obj.encrypt(pad(plaintext, AES.block_size))
-print(cyphertext1.hex())
+cyphertext = iv + Encrypt_obj.encrypt(pad(plaintext, AES.block_size))
+cyphertext1 = iv + Encrypt_obj1.encrypt(pad(plaintext, AES.block_size))
 
-Dec_obj = AES.new(key, AES.MODE_CBC, iv)
-plaintext2 = iv + unpad(Dec_obj.decrypt(cyphertext1), AES.block_size)
+Dec_obj = AES.new(key, AES.MODE_CFB, iv)
+Dec_obj2 = AES.new(key, AES.MODE_CBC, iv)
 
-print(str(plaintext2))
+plaintextcfb = iv + unpad(Dec_obj.decrypt(cyphertext), AES.block_size)
+plaintextcbc = iv + unpad(Dec_obj2.decrypt(cyphertext1), AES.block_size)
+
+print("CBC: " + str(plaintextcbc))
+print("CFB: " + str(plaintextcfb)) 
